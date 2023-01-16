9e2dd3cc6093681fa875c210009a7251dd11f2bd
Yann Simon <yann.simon@commercetools.com>
2023-01-16 11:09:40 +0100

    data is not present for errors before execution

--- src/test/scala/sangria/relay/util/ResultHelper.scala
+++ src/test/scala/sangria/relay/util/ResultHelper.scala
@@ -2,7 +2,8 @@ package sangria.relay.util
 
 trait ResultHelper {
   implicit class Helper(any: Any) {
-    def getProp(name: String) = any.asInstanceOf[Map[String, Any]](name)
+    def asMap: Map[String, Any] = any.asInstanceOf[Map[String, Any]]
+    def getProp(name: String) = asMap(name)
     def asList = any.asInstanceOf[Vector[Any]]
     def asString = any.asInstanceOf[String]
     def asAnyRef = any.asInstanceOf[AnyRef]
